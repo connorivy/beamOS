bb8e04a56dda6ebd73b7f5cafa347154d4dfdab8
Connor Ivy <connor@speckle.systems>
2022-12-15 21:28:46 -0600

    add multispan and sections

---
+++ beamOS.Tests/TestObjects/Element1Ds/MultipleSpan.cs
@@ -0,0 +1,14 @@
+﻿using Objects.Structural.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beamOS.Tests.TestObjects.Element1Ds
+{
+  internal class MultipleSpan
+  {
+    //public Element1D TwoSpans()
+  }
+}
