ce57495d6a4be2230f498c97d603ce9097bc8644
connorivy <43247197+connorivy@users.noreply.github.com>
2022-12-20 17:19:04 -0600

    Add data validation class (#2)
    * add single span element 1d definitions to fill in later
    
    * add multispan and sections
    
    * added data validation class
    
    Co-authored-by: Connor Ivy <connor@speckle.systems>

---
+++ beamOS.API/DataValidation.cs
@@ -0,0 +1,45 @@
+﻿namespace beamOS.API
+{
+  public class DataValidation
+  {
+    public void AssertNumber(object num, string varName)
+    {
+      var numericTypes = new HashSet<Type>()
+      {
+        typeof(byte),
+        typeof(double),
+        typeof(decimal),
+        typeof(float),
+        typeof(int),
+        typeof(long),
+        typeof(sbyte),
+        typeof(short),
+        typeof(uint),
+        typeof(ulong),
+        typeof(ushort),
+      };
+      if (!numericTypes.Contains(num.GetType()))
+        throw new Exception($"The value for {varName} should be of type int, long, float, or double. Not of type {num.GetType().FullName}");
+    }
+    public void AssertPositiveNumber(object num, string varName)
+    {
+      if (num is IConvertible)
+      {
+        if (Convert.ToDouble(num) >= 0.0)
+          return;
+        throw new Exception($"Number \"{num}\" is not greater than or equal to 0");
+      }
+      throw new Exception($"Number \"{num}\" is not of the correct type");
+    }
+    public void AssertStrictlyPositiveNumber(object num, string varName)
+    {
+      if (num is IConvertible)
+      {
+        if (Convert.ToDouble(num) > 0.0)
+          return;
+        throw new Exception($"Number \"{num}\" is not strictly greater than 0");
+      }
+      throw new Exception($"Number \"{num}\" is not of the correct type");
+    }
+  }
+}
---
+++ beamOS.API/Support.cs
@@ -0,0 +1,10 @@
+﻿namespace beamOS.API
+{
+  public class Support
+  {
+    public double Position { get; set; }
+    public double[] Stiffness { get; set; }
+    public bool[] DOFs { get; set; }
+    public bool[] Fixity { get; set; }
+  }
+}
