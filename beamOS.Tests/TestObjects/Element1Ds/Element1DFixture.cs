aa0b2f7f98ff7a2d87e07397aeb7105b13c5fe4d
Connor Ivy <connor@speckle.systems>
2023-04-13 20:45:48 -0500

    cleanup

--- beamOS.Tests/TestObjects/Element1Ds/Element1DFixture.cs
+++ beamOS.Tests/TestObjects/Element1Ds/Element1DFixture.cs
@@ -1,72 +1,66 @@
-﻿using beamOS.API.Schema.Objects;
+namespace beamOS.Tests.TestObjects.Element1Ds;
+using beamOS.API.Schema.Objects;
 using beamOS.Tests.TestObjects.SolvedProblems;
 using LanguageExt;
 using MathNet.Numerics.LinearAlgebra;
 
-namespace beamOS.Tests.TestObjects.Element1Ds
+public class AllElement1DFixtures : TheoryData<Element1DFixture>
 {
-
-  public class AllElement1DFixtures : TheoryData<Element1DFixture>
+  public List<Element1DFixture> Element1DFixtures { get; } = new()
   {
-    public List<Element1DFixture> Element1DFixtures = new()
+    // add standalone element 1D fixtures here
+  };
+  public AllElement1DFixtures()
+  {
+    var allSolvedProblems = new AllSolvedProblems();
+    foreach (var solvedProblem in allSolvedProblems.AllTestObjects)
     {
-      // add standalone element 1D fixtures here
-    };
-    public AllElement1DFixtures() 
-    {
-      var allSolvedProblems = new AllSolvedProblems();
-      foreach (var solvedProblem in allSolvedProblems.AllTestObjects)
+      foreach (var element1DFixture in solvedProblem.Element1DFixtures)
       {
-        foreach (var element1DFixture in solvedProblem.Element1DFixtures)
-        {
-          Add(element1DFixture);
-        }
-      }
-
-      foreach (var element1DFixture in Element1DFixtures)
-      {
-        Add(element1DFixture);
+        this.Add(element1DFixture);
       }
     }
-  }
 
-  public class Element1DFixture : SerializableFixtureBase, IHasLocalResults, IHasGlobalResults
-  {
-    public Element1DFixture()
+    foreach (var element1DFixture in this.Element1DFixtures)
     {
+      this.Add(element1DFixture);
     }
-    public Element1DFixture(Element1D element)
-    {
-      Element = element;
-    }
-    public Element1D Element { get; set; }
-    public Option<Matrix<double>> ExpectedRotationMatrix { get; set; }
-    public Option<Matrix<double>> ExpectedTransformationMatrix { get; set; }
-    public Option<Matrix<double>> ExpectedLocalStiffnessMatrix { get; set; }
-    public Option<Matrix<double>> ExpectedGlobalStiffnessMatrix { get; set; }
-    public Option<Vector<double>> ExpectedLocalFixedEndForces { get; set; }
-    public Option<Vector<double>> ExpectedGlobalFixedEndForces { get; set; }
-    public Option<Vector<double>> ExpectedLocalEndDisplacements { get; set; }
-    public Option<Vector<double>> ExpectedGlobalEndDisplacements { get; set; }
-    public Option<Vector<double>> ExpectedLocalEndForces { get; set; }
-    public Option<Vector<double>> ExpectedGlobalEndForces { get; set; }
   }
+}
 
-  public interface IHasLocalResults
+public class Element1DFixture : SerializableFixtureBase, IHasLocalResults, IHasGlobalResults
+{
+  public Element1DFixture()
   {
-    public Option<Matrix<double>> ExpectedRotationMatrix { get; set; }
-    public Option<Matrix<double>> ExpectedTransformationMatrix { get; set; }
-    public Option<Matrix<double>> ExpectedLocalStiffnessMatrix { get; set; }
-    public Option<Vector<double>> ExpectedLocalFixedEndForces { get; set; }
-    public Option<Vector<double>> ExpectedLocalEndDisplacements { get; set; }
-    public Option<Vector<double>> ExpectedLocalEndForces { get; set; }
   }
+  public Element1DFixture(Element1D element) => this.Element = element;
+  public Element1D Element { get; set; }
+  public Option<Matrix<double>> ExpectedRotationMatrix { get; set; }
+  public Option<Matrix<double>> ExpectedTransformationMatrix { get; set; }
+  public Option<Matrix<double>> ExpectedLocalStiffnessMatrix { get; set; }
+  public Option<Matrix<double>> ExpectedGlobalStiffnessMatrix { get; set; }
+  public Option<Vector<double>> ExpectedLocalFixedEndForces { get; set; }
+  public Option<Vector<double>> ExpectedGlobalFixedEndForces { get; set; }
+  public Option<Vector<double>> ExpectedLocalEndDisplacements { get; set; }
+  public Option<Vector<double>> ExpectedGlobalEndDisplacements { get; set; }
+  public Option<Vector<double>> ExpectedLocalEndForces { get; set; }
+  public Option<Vector<double>> ExpectedGlobalEndForces { get; set; }
+}
 
-  public interface IHasGlobalResults
-  {
-    public Option<Matrix<double>> ExpectedGlobalStiffnessMatrix { get; set; }
-    public Option<Vector<double>> ExpectedGlobalFixedEndForces { get; set; }
-    public Option<Vector<double>> ExpectedGlobalEndDisplacements { get; set; }
-    public Option<Vector<double>> ExpectedGlobalEndForces { get; set; }
-  }
+public interface IHasLocalResults
+{
+  public Option<Matrix<double>> ExpectedRotationMatrix { get; set; }
+  public Option<Matrix<double>> ExpectedTransformationMatrix { get; set; }
+  public Option<Matrix<double>> ExpectedLocalStiffnessMatrix { get; set; }
+  public Option<Vector<double>> ExpectedLocalFixedEndForces { get; set; }
+  public Option<Vector<double>> ExpectedLocalEndDisplacements { get; set; }
+  public Option<Vector<double>> ExpectedLocalEndForces { get; set; }
+}
+
+public interface IHasGlobalResults
+{
+  public Option<Matrix<double>> ExpectedGlobalStiffnessMatrix { get; set; }
+  public Option<Vector<double>> ExpectedGlobalFixedEndForces { get; set; }
+  public Option<Vector<double>> ExpectedGlobalEndDisplacements { get; set; }
+  public Option<Vector<double>> ExpectedGlobalEndForces { get; set; }
 }
