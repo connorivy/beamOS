6d1c44a0ec319164d57afabafd32755095648108
connorivy <43247197+connorivy@users.noreply.github.com>
2023-08-07 19:41:28 -0500

    git rid of more options (#12)
    Co-authored-by: Connor Ivy <connor@speckle.systems>

--- beamOS.Tests/TestObjects/SerializableFixtureBase.cs
+++ beamOS.Tests/TestObjects/SerializableFixtureBase.cs
@@ -1,8 +1,7 @@
 namespace beamOS.Tests.TestObjects;
+
+using System.Collections;
 using System.Reflection;
-using beamOS.API.Schema.Objects;
-using LanguageExt;
-using LanguageExt.UnsafeValueAccess;
 using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.LinearAlgebra.Double;
 using Speckle.Core.Models;
@@ -33,7 +32,7 @@ public class SerializableFixtureBase : IXunitSerializable
     {
       prop.SetValue(this, deserializerV2.Deserialize(info.GetValue<string>(prop.Name)));
     }
-    else if (type == typeof(Option<Matrix<double>>))
+    else if (type == typeof(Matrix<double>))
     {
       var matrixArray = info.GetValue<double[,]>(prop.Name);
       if (matrixArray == null)
@@ -41,10 +40,10 @@ public class SerializableFixtureBase : IXunitSerializable
         return;
       }
 
-      Option<Matrix<double>> matrixOption = DenseMatrix.OfArray(matrixArray);
+      var matrixOption = DenseMatrix.OfArray(matrixArray);
       prop.SetValue(this, matrixOption);
     }
-    else if (type == typeof(Option<Vector<double>>))
+    else if (type == typeof(Vector<double>))
     {
       var vectorArray = info.GetValue<double[]>(prop.Name);
       if (vectorArray == null)
@@ -52,19 +51,8 @@ public class SerializableFixtureBase : IXunitSerializable
         return;
       }
 
-      Option<Vector<double>> vectorOption = Vector<double>.Build.DenseOfArray(vectorArray);
+      var vectorOption = Vector<double>.Build.DenseOfArray(vectorArray);
       prop.SetValue(this, vectorOption);
-    }
-    else if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Option<>))
-    {
-      var genericTypeArgument = type.GenericTypeArguments[0];
-      var someType = typeof(Some<>);
-      var totalSomeType = someType.MakeGenericType(genericTypeArgument);
-
-      var val = info.GetValue<object>(prop.Name);
-      var instance = Activator.CreateInstance(totalSomeType, new[] { val });
-      var optionInstance = Activator.CreateInstance(type, new[] { instance });
-      prop.SetValue(this, optionInstance);
     }
     else if (type == typeof(string))
     {
@@ -91,6 +79,11 @@ public class SerializableFixtureBase : IXunitSerializable
       var stringValue = info.GetValue<bool>(prop.Name);
       prop.SetValue(this, stringValue);
     }
+    else if (type == typeof(double[]))
+    {
+      var stringValue = info.GetValue<double[]>(prop.Name);
+      prop.SetValue(this, stringValue);
+    }
   }
 
   public void Serialize(IXunitSerializationInfo info)
@@ -107,81 +100,37 @@ public class SerializableFixtureBase : IXunitSerializable
 
       switch (item)
       {
+        case Base @base:
+          info.AddValue(prop.Name, serializerV2.Serialize(@base));
+          continue;
+        case Matrix<double> matrix:
+          if (matrix != null)
+          {
+            info.AddValue(prop.Name, matrix.AsArray() ?? matrix.ToArray());
+          }
+          continue;
+        case Vector<double> vector:
+          if (vector != null)
+          {
+            info.AddValue(prop.Name, vector.AsArray() ?? vector.ToArray());
+          }
+          continue;
         case string:
         case int:
         case double:
         case float:
         case bool:
+        case IList:
           info.AddValue(prop.Name, item);
-          continue;
-        case Base @base:
-          info.AddValue(prop.Name, serializerV2.Serialize(@base));
-          continue;
-        case Option<Matrix<double>> matrix:
-          var x = matrix.ValueUnsafe();
-          _ = matrix.IfSome(
-            m => info.AddValue(prop.Name, m.AsArray() ?? m.ToArray())
-          );
-          continue;
-        case Option<Vector<double>> vector:
-          _ = vector.IfSome(
-            v => info.AddValue(prop.Name, v.AsArray() ?? v.ToArray())
-          );
           continue;
         default:
           break;
       }
-      TrySerializeOption(info, prop, item);
     }
     //}
     //catch (Exception e )
     //{
 
     //}
-  }
-
-  private static void TrySerializeOption(IXunitSerializationInfo info, PropertyInfo prop, object item)
-  {
-    var type = item.GetType();
-    if (!type.IsGenericType)
-    {
-      return;
-    }
-
-    var genericType = type.GetGenericTypeDefinition();
-
-    if (genericType != typeof(Option<>))
-    {
-      return;
-    }
-
-    var genericTypeArgument = type.GenericTypeArguments[0];
-    var methods = typeof(UnsafeValueAccessExtensions)
-      .GetMethods()
-      .Where(m => m.Name == "ValueUnsafe");
-
-    MethodInfo? genericMethod = null;
-    foreach (var method in methods)
-    {
-      genericMethod = method.MakeGenericMethod(genericTypeArgument);
-      if (genericMethod != null)
-      {
-        break;
-      }
-    }
-
-    if (genericMethod == null)
-    {
-      return;
-    }
-
-    var value = genericMethod.Invoke(null, new object[] { item });
-
-    if (value == null)
-    {
-      return;
-    }
-
-    info.AddValue(prop.Name, value);
   }
 }
