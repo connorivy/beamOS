299c87ac1a9294bb8919fd99ed70efb3dc1bc631
Connor Ivy <connor@speckle.systems>
2023-11-27 07:58:36 -0600

    reorg sln

--- src/BeamOS.DirectStiffnessMethod/BeamOS.DirectStiffnessMethod.Api/AnalyticalModels/Endpoints/RunDirectStiffnessMethodEndpoint.cs
+++ src/BeamOS.DirectStiffnessMethod/BeamOS.DirectStiffnessMethod.Api/AnalyticalModels/Endpoints/RunDirectStiffnessMethodEndpoint.cs
@@ -23,6 +23,6 @@ public class RunDirectStiffnessMethodEndpoint(
 
         AnalyticalModel model = await createAnalyticalModelCommandHandler.ExecuteAsync(command, ct);
 
-        return null;
+        return "success";
     }
 }
