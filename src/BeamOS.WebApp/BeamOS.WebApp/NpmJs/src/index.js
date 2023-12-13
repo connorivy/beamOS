61f4d301411257c36b35426355486238e262d0b7
Connor Ivy <connor@speckle.systems>
2023-12-12 18:12:00 -0600

    wip with new editor

--- src/BeamOS.WebApp/BeamOS.WebApp/NpmJs/src/index.js
+++ src/BeamOS.WebApp/BeamOS.WebApp/NpmJs/src/index.js
@@ -1,3 +1,12 @@
 import { BeamOsEditor } from 'beamos-editor';
 
-BeamOsEditor.Register();
+window.createFromId = (editorId) => {
+  window.beamOsEditor = BeamOsEditor.createFromId(editorId)
+  //window.editor.animate()
+}
+
+window.interopFunctions = {
+  clickElement: function (element) {
+    element.click();
+  }
+}
