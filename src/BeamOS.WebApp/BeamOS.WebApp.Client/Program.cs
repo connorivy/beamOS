4ff8e0f062276b42f6ccd304d74d78a841f3454c
Connor Ivy <connor@speckle.systems>
2023-12-14 18:10:05 -0600

    interop working with semi type saftey

--- src/BeamOS.WebApp/BeamOS.WebApp.Client/Program.cs
+++ src/BeamOS.WebApp/BeamOS.WebApp.Client/Program.cs
@@ -1,19 +1,19 @@
-using BeamOS.PhysicalModel.Clients.Cs;
-using BeamOS.PhysicalModel.Clients.Cs.Extensions;
+//using BeamOS.PhysicalModel.Clients.Cs;
+//using BeamOS.PhysicalModel.Clients.Cs.Extensions;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder
-    .Services
-    .AddHttpClient<PhysicalModelAlphaClientFactory>(
-        client => client.BaseAddress = new("https://localhost:7193")
-    );
+//builder
+//    .Services
+//    .AddHttpClient<PhysicalModelAlphaClientFactory>(
+//        client => client.BaseAddress = new("https://localhost:7193")
+//    );
 
-builder
-    .Services
-    .AddScoped<PhysicalModelAlphaClient>(
-        x => x.GetRequiredService<PhysicalModelAlphaClientFactory>().Create()
-    );
+//builder
+//    .Services
+//    .AddScoped<PhysicalModelAlphaClient>(
+//        x => x.GetRequiredService<PhysicalModelAlphaClientFactory>().Create()
+//    );
 
 await builder.Build().RunAsync();
