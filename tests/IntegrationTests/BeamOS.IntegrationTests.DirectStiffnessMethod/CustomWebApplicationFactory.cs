fbab5ba84b60e5383aa679da73c5c5b9858e03eb
Connor Ivy <connor@speckle.systems>
2024-03-25 18:33:47 -0500

    start sql server during tests

--- tests/IntegrationTests/BeamOS.IntegrationTests.DirectStiffnessMethod/CustomWebApplicationFactory.cs
+++ tests/IntegrationTests/BeamOS.IntegrationTests.DirectStiffnessMethod/CustomWebApplicationFactory.cs
@@ -1,11 +1,10 @@
 using System.Data.Common;
 using BeamOs.Infrastructure;
-using FastEndpoints.Testing;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace BeamOS.IntegrationTests.DirectStiffnessMethod;
 
@@ -48,4 +47,43 @@ public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProg
 
     //    builder.UseEnvironment("Development");
     //}
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        if (
+            !bool.TryParse(
+                Environment.GetEnvironmentVariable("ContinuousIntegrationBuild"),
+                out bool isCiBuild
+            ) || !isCiBuild
+        )
+        {
+            base.ConfigureWebHost(builder);
+            return;
+        }
+
+        // if this is a ci build, then replace the default connection string
+        builder.ConfigureServices(services =>
+        {
+            var dbContextDescriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<BeamOsStructuralDbContext>)
+            );
+
+            services.Remove(dbContextDescriptor);
+
+            var dbConnectionDescriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbConnection)
+            );
+
+            services.Remove(dbConnectionDescriptor);
+
+            const string connectionString =
+                "Server=localhost,1433;Database=yourDatabaseName;Integrated Security=False;Encrypt=False;TrustServerCertificate=true;MultipleActiveResultSets=true;User=SA;Password=yourStrong(!)Password;";
+
+            services.AddDbContext<BeamOsStructuralDbContext>(
+                options => options.UseSqlServer(connectionString)
+            );
+        });
+
+        builder.UseEnvironment("Development");
+    }
 }
