b000b4c2f133fba2320bd2c0ee270aeb7272f8d9
Connor Ivy <connorivy15@utexas.edu>
2025-01-29 15:13:13 -0600

    add test result display view

---
+++ src/WebApp/BeamOs.WebApp.Components/Features/TestExplorer/AssertionResultVectorView.razor.cs
@@ -0,0 +1,25 @@
+using BeamOs.WebApp.Client.Components.Features.TestExplorer;
+using Microsoft.AspNetCore.Components;
+
+namespace BeamOs.WebApp.Components.Features.TestExplorer;
+
+public partial class AssertionResultVectorView : ComponentBase
+{
+    [Parameter]
+    public required string ComparedValueName { get; init; }
+
+    [Parameter]
+    public required AssertionResult<double?[]> AssertionResultArray { get; init; }
+
+    [Parameter]
+    public ICollection<string>? ComparedValueNameCollection { get; init; }
+
+    private static double? GetDifferenceOrNull(double? expected, double? calculated)
+    {
+        if (expected is not double typedExpected || calculated is not double typedCalculated)
+        {
+            return null;
+        }
+        return Math.Round(typedExpected - typedCalculated, 5);
+    }
+}
