146a2129ea9eadf834d9db05b08d271687ed7965
Connor Ivy <connorivy15@utexas.edu>
2025-02-18 14:55:28 -0600

    init modelBuilder

--- tests/StructuralAnalysis/BeamOs.Tests.StructuralAnalysis.Integration/OpenSeesTests.cs
+++ tests/StructuralAnalysis/BeamOs.Tests.StructuralAnalysis.Integration/OpenSeesTests.cs
@@ -14,6 +14,7 @@ public partial class OpenSeesTests
 
         foreach (var modelBuilder in AllSolvedProblems.ModelFixtures())
         {
+            await modelBuilder.InitializeAsync();
             await modelBuilder.CreateOnly(AssemblySetup.StructuralAnalysisApiClient);
 
             await AssemblySetup
