f03b4880d597dd24ba309a26f6e3f2de2e03569e
Connor Ivy <connorivy15@utexas.edu>
2025-03-10 11:56:40 -0500

    add preview end

--- tests/BeamOs.Tests.Common/BeamOsEnv.cs
+++ tests/BeamOs.Tests.Common/BeamOsEnv.cs
@@ -2,11 +2,11 @@ namespace BeamOs.Tests.Common;
 
 public static class BeamOsEnv
 {
-    public static bool IsCiEnv()
-    {
-        return bool.TryParse(
-                Environment.GetEnvironmentVariable("ContinuousIntegrationEnv"),
-                out bool isCiEnv
-            ) && isCiEnv;
-    }
+    public static bool IsCiEnv() => GetBoolFromEnvVar("ContinuousIntegrationEnv");
+
+    public static bool IsPreviewEnv() => GetBoolFromEnvVar("BEAMOS_PREVIEW");
+
+    private static bool GetBoolFromEnvVar(string var) =>
+        bool.TryParse(Environment.GetEnvironmentVariable(var), out bool envVarAsBool)
+        && envVarAsBool;
 }
