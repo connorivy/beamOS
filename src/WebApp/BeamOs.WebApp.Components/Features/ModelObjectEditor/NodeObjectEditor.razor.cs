8fc25470a978d4672aa931404f9dd2e5a73b609b
Connor Ivy <connorivy15@utexas.edu>
2025-03-20 14:10:42 -0500

    tweak node ui to allow new node creation

--- src/WebApp/BeamOs.WebApp.Components/Features/ModelObjectEditor/NodeObjectEditor.razor.cs
+++ src/WebApp/BeamOs.WebApp.Components/Features/ModelObjectEditor/NodeObjectEditor.razor.cs
@@ -19,6 +19,57 @@ public partial class NodeObjectEditor(
 {
     private NodeModel node = new();
 
+    private int nodeId
+    {
+        get => this.node.Id;
+        set
+        {
+            if (value > 0)
+            {
+                this.UpdateFromNodeId(value);
+            }
+            this.node.Id = value;
+        }
+    }
+
+    private const int ResultLimit = 50;
+
+    private string nodeIdText => nodeId == 0 ? "New Node" : nodeId.ToString();
+
+    private List<string> nodeIds;
+    private static int[] nullInt = [0];
+    private Task<IEnumerable<int>> NodeIds(string searchText, CancellationToken ct)
+    {
+        if (!int.TryParse(searchText, out int subInt))
+        {
+            return Task.FromResult(nullInt.Concat(editorState.Value.CachedModelResponse.Nodes.Keys));
+        }
+
+        // Get the number of digits in the subInt
+        int subIntLength = GetNumberOfDigits(subInt);
+
+        return Task.FromResult(nullInt.Concat(editorState.Value.CachedModelResponse.Nodes.Keys.Where(k => GetPrefix(k, subIntLength) == subInt)));
+    }
+
+    // Helper method to get the number of digits in an integer
+    private static int GetNumberOfDigits(int number)
+    {
+        if (number == 0)
+            return 1; // Edge case for 0
+        return (int)Math.Floor(Math.Log10(Math.Abs(number))) + 1;
+    }
+
+    // Helper method to get the prefix of a number with a specified length
+    private static int GetPrefix(int number, int prefixLength)
+    {
+        int numberOfDigits = GetNumberOfDigits(number);
+        if (prefixLength >= numberOfDigits)
+        {
+            return number; // The entire number is the prefix
+        }
+        return number / (int)Math.Pow(10, numberOfDigits - prefixLength);
+    }
+
     [Parameter]
     public UnitSettingsContract UnitSettings { get; set; }
 
@@ -48,9 +99,14 @@ public partial class NodeObjectEditor(
 
         if (this.SelectedObject is not null && this.SelectedObject.TypeName == "Node")
         {
-            var response = editorState.Value.CachedModelResponse.Nodes[this.SelectedObject.Id];
-            this.UpdateFromNodeResponse(response);
+            UpdateFromNodeId(this.SelectedObject.Id);
         }
+    }
+
+    private void UpdateFromNodeId(int nodeId)
+    {
+        var response = editorState.Value.CachedModelResponse.Nodes[nodeId];
+        this.UpdateFromNodeResponse(response);
     }
 
     private void UpdateFromNodeResponse(NodeResponse response)
@@ -82,13 +138,13 @@ public partial class NodeObjectEditor(
 
     private async Task Submit()
     {
-        PutNodeCommand command = new() { Id = node.Id.Value, ModelId = node.ModelId, Body = new NodeData() { LocationPoint = new(node.LocationPoint.X.Value, node.LocationPoint.Y.Value, node.LocationPoint.Z.Value, UnitSettings.LengthUnit), Restraint = new(node.Restraint.CanTranslateAlongX, node.Restraint.CanTranslateAlongY, node.Restraint.CanTranslateAlongZ, node.Restraint.CanRotateAboutX, node.Restraint.CanRotateAboutY, node.Restraint.CanRotateAboutZ) } };
+        PutNodeCommand command = new() { Id = node.Id, ModelId = node.ModelId, Body = new NodeData() { LocationPoint = new(node.LocationPoint.X.Value, node.LocationPoint.Y.Value, node.LocationPoint.Z.Value, UnitSettings.LengthUnit), Restraint = new(node.Restraint.CanTranslateAlongX, node.Restraint.CanTranslateAlongY, node.Restraint.CanTranslateAlongZ, node.Restraint.CanRotateAboutX, node.Restraint.CanRotateAboutY, node.Restraint.CanRotateAboutZ) } };
         await putNodeCommandHandler.ExecuteAsync(command);
     }
 
     public class NodeModel
     {
-        public int? Id { get; set; }
+        public int Id { get; set; }
         public Guid ModelId { get; set; }
         public PointModel LocationPoint { get; set; } = new();
         public RestraintModel Restraint { get; set; } = new();
