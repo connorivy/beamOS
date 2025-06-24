e7701ed30a078be8b772a27d5d1f431f4c1f1702
Connor Ivy <connorivy15@utexas.edu>
2025-06-24 12:08:11 -0500

    fix devcontainer

---
+++ scripts/devcontainer-init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This script runs after the dev container is created to set up the workspace.
+
+set -e
+
+# Make sure deepClean is executable and run it
+chmod +x /workspaces/beamOS/deepClean
+/workspaces/beamOS/deepClean
+
+# Restore .NET dependencies
+cd /workspaces/beamOS
+dotnet restore
+
+# Install npm dependencies for BeamOs.WebApp.Components
+cd /workspaces/beamOS/src/WebApp/BeamOs.WebApp.Components
+npm install
