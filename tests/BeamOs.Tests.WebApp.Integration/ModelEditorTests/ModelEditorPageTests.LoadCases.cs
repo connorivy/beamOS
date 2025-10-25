8a524679866260c536c80ed0680959e3a6c4966e
connorivy <connorivy15@utexas.edu>
2025-10-24 19:59:03 -0500

    add failing test for load case

--- tests/BeamOs.Tests.WebApp.Integration/ModelEditorTests/ModelEditorPageTests.LoadCases.cs
+++ tests/BeamOs.Tests.WebApp.Integration/ModelEditorTests/ModelEditorPageTests.LoadCases.cs
@@ -29,9 +29,7 @@ public partial class ModelEditorPageTests : ReactPageTest
         );
         await this.Expect(dropdownOptions).ToHaveCountAsync(0);
 
-        // fill in value for load case name
-        var nameInput = this.Page.GetByRole(AriaRole.Textbox, new() { Name = "name" });
-        await nameInput.FillAsync("Test Load Case");
+        await FillOutLoadCaseSelectionInfo(this.Page, "Test Load Case");
 
         // click the create button
         var createButton = this.Page.GetByRole(AriaRole.Button, new() { Name = "create" });
@@ -50,14 +48,14 @@ public partial class ModelEditorPageTests : ReactPageTest
         // refresh the page and ensure the created node persists
         await this.Page.ReloadAsync();
 
-        // click the nodes tab in the sidebar again
+        // click the load cases tab in the sidebar again
         entityTab = this.Page.GetByRole(
             AriaRole.Button,
             new PageGetByRoleOptions { Name = "load cases" }
         );
         await entityTab.ClickAsync();
 
-        // insert 1 into the node id combobox again
+        // insert 1 into the load case id combobox again
         await idCombobox.FillAsync("1");
         await idCombobox.ClickAsync();
 
@@ -68,10 +66,115 @@ public partial class ModelEditorPageTests : ReactPageTest
         );
         await this.Expect(dropdownOptions).ToHaveCountAsync(1);
 
-        // select the node from the dropdown
+        // select the load case from the dropdown
         await dropdownOptions.First.ClickAsync();
 
         // verify that the load case name input has the correct value
-        await this.Expect(nameInput).ToHaveValueAsync("Test Load Case");
+        await AssertLoadCaseDialogValues(this.Page, "Test Load Case");
+    }
+
+    [Test]
+    [DependsOn(nameof(LoadCaseDialog_ShouldCreateLoadCase))]
+    public async Task LoadCaseDialog_ShouldModifyExistingLoadCase()
+    {
+        // click the loadCases tab in the sidebar
+        var loadCasesTab = this.Page.GetByRole(
+            AriaRole.Button,
+            new PageGetByRoleOptions { Name = "load case" }
+        );
+        await loadCasesTab.ClickAsync();
+
+        await FillOutLoadCaseSelectionInfo(this.Page, "new load case name", "1");
+
+        // click the apply button
+        var applyButton = this.Page.GetByRole(AriaRole.Button, new() { Name = "apply" });
+        await applyButton.ClickAsync();
+
+        var loadCaseIdCombobox = this.Page.GetByRole(
+            AriaRole.Combobox,
+            new PageGetByRoleOptions { Name = "id" }
+        );
+        await this.Expect(loadCaseIdCombobox).ToHaveValueAsync("1");
+
+        // test the optimistic store changes by reloading the element data without refreshing the page
+        await loadCaseIdCombobox.ClickAsync();
+        var clearButton = this.Page.GetByRole(AriaRole.Button, new() { Name = "clear" });
+        await clearButton.ClickAsync();
+
+        await loadCaseIdCombobox.FillAsync("1");
+        var dropdownOptions = this.Page.GetByRole(
+            AriaRole.Option,
+            new PageGetByRoleOptions { Name = "1" }
+        );
+        await this.Expect(dropdownOptions).ToHaveCountAsync(1);
+        await dropdownOptions.First.ClickAsync();
+
+        await AssertLoadCaseDialogValues(this.Page, "1", "1");
+
+        // test the database changes by reloading the element data from the server by refreshing the page
+        await this.Page.ReloadAsync();
+
+        // click the loadCases tab in the sidebar again
+        loadCasesTab = this.Page.GetByRole(
+            AriaRole.Button,
+            new PageGetByRoleOptions { Name = "load case" }
+        );
+        await loadCasesTab.ClickAsync();
+
+        // insert 1 into the loadCase id combobox again
+        await loadCaseIdCombobox.FillAsync("1");
+        await loadCaseIdCombobox.ClickAsync();
+
+        // now there should be one result in the dropdown
+        dropdownOptions = this.Page.GetByRole(
+            AriaRole.Option,
+            new PageGetByRoleOptions { Name = "1" }
+        );
+        await this.Expect(dropdownOptions).ToHaveCountAsync(1);
+
+        // select the loadCase from the dropdown
+        await dropdownOptions.First.ClickAsync();
+
+        await AssertLoadCaseDialogValues(this.Page, "new load case name", "1");
+    }
+
+    internal static async Task FillOutLoadCaseSelectionInfo(
+        IPage page,
+        string name,
+        string? loadCaseId = null
+    )
+    {
+        if (loadCaseId is not null)
+        {
+            var loadCaseIdInput = page.GetByRole(AriaRole.Combobox, new() { Name = "id" });
+            await loadCaseIdInput.FillAsync(loadCaseId);
+
+            // select the option in the dropdown
+            var dropdownOption = page.GetByRole(AriaRole.Option, new() { Name = loadCaseId });
+            await dropdownOption.ClickAsync();
+        }
+
+        var nameInput = page.GetByRole(AriaRole.Textbox, new() { Name = "name" });
+        await nameInput.FillAsync(name);
+    }
+
+    internal static async Task AssertLoadCaseDialogValues(
+        IPage page,
+        string name,
+        string? loadCaseId = null
+    )
+    {
+        if (loadCaseId is not null)
+        {
+            var loadCaseIdInput = page.GetByRole(AriaRole.Combobox, new() { Name = "id" });
+            await loadCaseIdInput.FillAsync(loadCaseId);
+
+            // select the option in the dropdown
+            var dropdownOption = page.GetByRole(AriaRole.Option, new() { Name = loadCaseId });
+            await dropdownOption.ClickAsync();
+        }
+
+        var nameInput = page.GetByRole(AriaRole.Textbox, new() { Name = "name" });
+        await Assertions.Expect(nameInput).ToHaveValueAsync(name);
     }
 }
